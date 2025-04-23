d236ea6ebda033e8c9832436505abdd430941190
gersonjustiniano <gersonjustiniano21@gmail.com>
2025-04-23 15:39:45 -0400

    Primer commit

---
+++ plot_random_prices.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+var={'time':[],'close':[]}
+
+for i in range(200):
+	var['time'].append(str(i+1))
+	if len(var['close'])==0:
+		var['close'].append(random.uniform(1,2))
+	else:
+		up=var['close'][-1]*(1+0.01)
+		down=var['close'][-1]*(1-0.01)
+		var['close'].append(random.uniform(down,up))
+	print(var['time'][-1],var['close'][-1])
+
+fig=plt.figure()
+ax=fig.add_subplot()
+
+ax.clear()
+ax.set_xticks([])
+ax.plot(var['time'],var['close'])
+
+plt.show()
