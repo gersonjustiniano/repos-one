08b47f801af399f070380a6459a08b00d735f35a
gersonjustiniano <gersonjustiniano21@gmail.com>
2025-04-23 16:45:27 -0400

    add new color to line

--- plot_random_prices.py
+++ plot_random_prices.py
@@ -4,7 +4,7 @@ import random
 
 var={'time':[],'close':[]}
 
-for i in range(200):
+for i in range(100):
 	var['time'].append(str(i+1))
 	if len(var['close'])==0:
 		var['close'].append(random.uniform(1,2))
@@ -12,13 +12,13 @@ for i in range(200):
 		up=var['close'][-1]*(1+0.01)
 		down=var['close'][-1]*(1-0.01)
 		var['close'].append(random.uniform(down,up))
-	print(var['time'][-1],var['close'][-1])
+	print(len(var['time']),var['time'][-1],var['close'][-1])
 
 fig=plt.figure()
 ax=fig.add_subplot()
 
 ax.clear()
 ax.set_xticks([])
-ax.plot(var['time'],var['close'])
+ax.plot(var['time'],var['close'],c='black',lw=1)
 
 plt.show()
